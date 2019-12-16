72e2f342da01790dac28938e3e2965216d12c8de
cseiasp <caroline.seilern@hotmail.com>
2019-12-16 20:52:55 +0000

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,10 +3,20 @@ import { movies } from '../data';
 
 const Movies = () => {
   return (
+
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map( (movie, index) => 
+      
+      <div key = {index}>
+      <p>Title: {movie.title}</p>
+      <p>Time: {movie.time}</p>
+      <ul>Genres:{movie.genres.map(genre => 
+        <li>{genre}</li>)}</ul>
+      </div>
+      )}
     </div>
-  );
+  )
 };
 
 export default Movies;
