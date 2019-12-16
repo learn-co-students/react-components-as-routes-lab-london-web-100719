3f59fd34b9a7a794d3d1f013ade2614643915c87
Liko <li_xie1992@hotmail.com>
2019-12-16 12:39:02 +0000

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import { actors } from '../data';
+import React from "react";
+import { actors } from "../data";
 
 const Actors = () => {
+  const showActors = () => {
+    return actors.map(a => {
+      return (
+        <div>
+          <h3>{a.name}</h3>
+          <ul>
+            {a.movies.map(m => (
+              <li>{m}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    });
+  };
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {showActors()}
     </div>
   );
 };
