56671512a9423b8900aa4aa180319e7c89ea135d
Ned-el-ch <nedelchev.niki@gmail.com>
2020-01-02 11:30:39 +0000

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,11 +2,23 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
-  return (
-    <div>
-      {/*{code here}*/}
-    </div>
-  );
+	return (
+		<div>
+			<h1>Directors Page</h1>
+			{directors.map(director => {
+				return (
+					<div>
+						<h2>{director.name}</h2>
+						<ul>
+							{director.movies.map(movie => {
+								return (<li>{movie}</li>)
+							})}
+						</ul>
+					</div>
+				)
+			})}
+		</div>
+	);
 }
 
 export default Directors
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+const link = {
+	background: 'lightblue'
+}
+
 const NavBar = () => {
-  return (
-    <div>
-      {/*{code here}*/}
-    </div>
-  );
+	return (
+		<div className='navbar'>
+			< NavLink
+				to='/'
+				exact
+				style={link}
+				activeStyle={{background: 'blue'}}
+			>Home</NavLink>
+			< NavLink
+				to='/actors'
+				exact
+				style={link}
+				activeStyle={{background: 'blue'}}
+			>Actors</NavLink>
+			< NavLink
+				to='/directors'
+				exact
+				style={link}
+				activeStyle={{background: 'blue'}}
+			>Directors</NavLink>
+
+			< NavLink
+				to='/movies'
+				exact
+				style={link}
+				activeStyle={{background: 'blue'}}
+			>Movies</NavLink>
+		</div>
+	);
 };
 
 export default NavBar;
